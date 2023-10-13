af9fbeb422fde6cab2b45d184a4f55b3836f01d4
kdrbipul <kdrbipul@gmail.com>
2023-10-14 02:55:10 +0600

    last commit today

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const express = require ('express')
-const app = express()
-const port = 5000
+const express = require ('express');
+const app = express();
+const port = 5000;
+const users = require('./product.json');
 
 app.get('/', (req, res)=>{
-    res.send('Welcome to my website')
-})
+    res.send('Welcome to my website');
+});
 
+app.get('/users', (req, res) => {
+    res.send(users);
+    // console.log(users);
+});
+
+app.get('/users/uid', (req, res) =>{
+    const id = req.params.uid;
+    console.log(id);
+})
 
 app.listen(port, () =>{
     console.log(`Our website run to : ${port}`);
-})
+});
